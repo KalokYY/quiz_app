511c4d5d014f40f0c6916c8c76354aba87540f57
Michelle Ng <kalokmng@gmail.com>
2025-09-25 11:34:22 -0400

    Lifted state for the quiz.dart. Created the initState()

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -15,6 +15,7 @@ class Quiz extends StatefulWidget{
 
 class _QuizState extends State<Quiz> {
   Widget ? activeScreen;
+  @override
   void initState() {
     activeScreen = StartScreen(switchScreeen);
     super.initState();
