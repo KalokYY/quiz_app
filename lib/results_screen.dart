53a719e52bec572b78a17b2dfeaf45f07ba9abd4
Michelle Ng <kalokmng@gmail.com>
2025-10-05 15:01:28 -0400

    Added score to the results_screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -24,6 +24,16 @@ class ResultsScreen extends StatelessWidget{
   @override
   Widget build(context)
   {
+    final summaryData = getSummaryData();
+
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where(
+    (data) 
+      {
+        return data['correct_answer'] == data['user_answer'];
+      }
+    ).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -31,7 +41,7 @@ class ResultsScreen extends StatelessWidget{
         child:  Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:  [
-            const Text('You answered X out of Y questions Correctly'),
+            Text('You answered $numCorrectQuestions out of $numTotalQuestions questions Correctly'),
             const SizedBox(
               height:30,
             ),
